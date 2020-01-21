6f135ea693cbabd1a9a322a251ba59ae751e0655
JuanBustosV <juanbv81@gmail.com>
2020-01-21 21:27:37 +0100

    Abierto cerrado I

---
+++ 03OpenClosed/CTienda.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03OpenClosed
+{
+    // La clase debería estar CERRADA para cambios, pero ABIERTA para extensión
+    class CTienda
+    {
+        private List<CProducto> productos;
+
+        public CTienda(List<CProducto> pProductos)
+        {
+            productos = pProductos;
+        }
+
+        public void calcularInventario()
+        {
+            double total = 0;
+
+            //foreach (CProducto producto in productos)
+            //{
+            //    Console.WriteLine(producto);
+            //    total += producto.Precio;
+            //}
+
+            // Cambio (en la clase! evitar)
+            foreach (CProducto producto in productos)
+            {
+                if (producto.Categoria == 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    producto.Precio *= 0.8;
+                    Console.WriteLine(producto);
+                    total += producto.Precio;
+                }
+                if (producto.Categoria == 2)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    producto.Precio *= 1.2;
+                    Console.WriteLine(producto);
+                    total += producto.Precio;
+                }
+            }
+            Console.ResetColor();
+
+            Console.WriteLine("\tEl precio total en inventario es {0} €", total);
+        }
+    }
+}
